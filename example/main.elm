d37670fa9b866a9a513297e1ca2ec0a258662a58
Tiziano Santoro <tiziano88@gmail.com>
2016-11-28 18:59:48 +0000

    Upgrade to 0.18 and reformat

--- example/main.elm
+++ example/main.elm
@@ -1,119 +1,121 @@
+module Main exposing (..)
+
 import Html exposing (..)
-import Html.App as Html
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Http
 import Navigation
 import Task
-
 import OAuth
 import OAuth.Config
 
 
 main =
-  Navigation.program (OAuth.urlParser digitalOceanAuthClient)
-    { init = init
-    , view = view
-    , update = update
-    , urlUpdate = urlUpdate
-    , subscriptions = subscriptions
-    }
+    Navigation.program
+        (always Nop)
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
 
 
 -- MODEL
 
 
 type alias Model =
-  { token : Maybe OAuth.Token
-  }
+    { token : Maybe OAuth.Token
+    }
+
 
 googleAuthClient : OAuth.Client
 googleAuthClient =
-  OAuth.newClient
-    OAuth.Config.google
-    { clientId = "253270339440-tp9fiqj5boaqvrs3j8g2u0mtdn4ittgp.apps.googleusercontent.com"
-    , scopes = [ "https://www.googleapis.com/auth/drive" ]
-    , redirectUrl = "http://localhost:8000/main.elm"
-    }
+    OAuth.newClient
+        OAuth.Config.google
+        { clientId = "253270339440-tp9fiqj5boaqvrs3j8g2u0mtdn4ittgp.apps.googleusercontent.com"
+        , scopes = [ "https://www.googleapis.com/auth/drive" ]
+        , redirectUrl = "http://localhost:8000/main.elm"
+        }
 
 
 facebookAuthClient : OAuth.Client
 facebookAuthClient =
-  OAuth.newClient
-    OAuth.Config.facebook
-    { clientId = "299210180425495"
-    , scopes = [ "public_profile" ]
-    , redirectUrl = "http://localhost:8000/main.elm"
-    }
+    OAuth.newClient
+        OAuth.Config.facebook
+        { clientId = "299210180425495"
+        , scopes = [ "public_profile" ]
+        , redirectUrl = "http://localhost:8000/main.elm"
+        }
 
 
 digitalOceanAuthClient : OAuth.Client
 digitalOceanAuthClient =
-  OAuth.newClient
-    OAuth.Config.digitalOcean
-    { clientId = "34047f70e9d7e91befef744b49f21fc58d506e8f0c1d7681e8af7fdb4d59da55"
-    , scopes = [ "read" ]
-    , redirectUrl = "http://localhost:8000/main.elm"
-    }
+    OAuth.newClient
+        OAuth.Config.digitalOcean
+        { clientId = "34047f70e9d7e91befef744b49f21fc58d506e8f0c1d7681e8af7fdb4d59da55"
+        , scopes = [ "read" ]
+        , redirectUrl = "http://localhost:8000/main.elm"
+        }
 
 
 gitHubAuthClient : OAuth.Client
 gitHubAuthClient =
-  OAuth.newClient
-    OAuth.Config.gitHub
-    { clientId = "b7941bb82bd63e684712"
-    , scopes = [ "user" ]
-    , redirectUrl = "http://localhost:8000/main.elm"
-    }
+    OAuth.newClient
+        OAuth.Config.gitHub
+        { clientId = "b7941bb82bd63e684712"
+        , scopes = [ "user" ]
+        , redirectUrl = "http://localhost:8000/main.elm"
+        }
 
 
 stackExchangeAuthClient : OAuth.Client
 stackExchangeAuthClient =
-  OAuth.newClient
-    OAuth.Config.stackExchange
-    { clientId = "7515"
-    , scopes = [ "" ]
-    , redirectUrl = "http://localhost:8000/main.elm"
-    }
+    OAuth.newClient
+        OAuth.Config.stackExchange
+        { clientId = "7515"
+        , scopes = [ "" ]
+        , redirectUrl = "http://localhost:8000/main.elm"
+        }
 
 
-init : (Task.Task String OAuth.Token) -> (Model, Cmd Msg)
-init task =
-  { token = Nothing
-  } ! [ Task.perform (always Nop) Token task ]
+init : Navigation.Location -> ( Model, Cmd Msg )
+init location =
+    { token = Nothing } ! [ OAuth.init googleAuthClient location |> Cmd.map Token ]
+
 
 
 -- UPDATE
 
 
 type Msg
-  = Nop
-  | Token OAuth.Token
-  | Drive
-  | DriveResp
+    = Nop
+    | Token (Result Http.Error OAuth.Token)
+    | Drive
+    | DriveResp
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Nop ->
-      model ! []
+    case msg of
+        Nop ->
+            model ! []
 
-    Token t ->
-      { model
-      | token = Just t
-      } ! [ Navigation.modifyUrl "#" ]
+        Token (Ok t) ->
+            { model
+                | token = Just t
+            }
+                ! [ Navigation.modifyUrl "#" ]
 
-    DriveResp ->
-      model ! []
+        Token (Err _) ->
+            model ! []
 
-    Drive ->
-      model ! [ driveCmd model ]
+        DriveResp ->
+            model ! []
 
+        Drive ->
+            model ! [ driveCmd model ]
 
-urlUpdate : (Task.Task String OAuth.Token) -> Model -> (Model, Cmd Msg)
-urlUpdate task model =
-  (model, Task.perform (always Nop) (always Nop) task)
 
 
 -- VIEW
@@ -121,22 +123,23 @@ urlUpdate task model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ h2 [] [text "OAuth Demo"]
-    , ul []
-      [ li [] [ a [ href <| OAuth.buildAuthUrl googleAuthClient ] [ text "Google" ] ]
-      , li [] [ a [ href <| OAuth.buildAuthUrl facebookAuthClient ] [ text "Facebook" ] ]
-      , li [] [ a [ href <| OAuth.buildAuthUrl digitalOceanAuthClient ] [ text "DigitalOcean" ] ]
-      , li [] [ a [ href <| OAuth.buildAuthUrl gitHubAuthClient ] [ text "GitHub" ] ]
-      , li [] [ a [ href <| OAuth.buildAuthUrl stackExchangeAuthClient ] [ text "StackExchange" ] ]
-      ]
-    , pre
-      [ style
-        [ ("white-space", "pre-wrap")
+    div []
+        [ h2 [] [ text "OAuth Demo" ]
+        , ul []
+            [ li [] [ a [ href <| OAuth.buildAuthUrl googleAuthClient ] [ text "Google" ] ]
+            , li [] [ a [ href <| OAuth.buildAuthUrl facebookAuthClient ] [ text "Facebook" ] ]
+            , li [] [ a [ href <| OAuth.buildAuthUrl digitalOceanAuthClient ] [ text "DigitalOcean" ] ]
+            , li [] [ a [ href <| OAuth.buildAuthUrl gitHubAuthClient ] [ text "GitHub" ] ]
+            , li [] [ a [ href <| OAuth.buildAuthUrl stackExchangeAuthClient ] [ text "StackExchange" ] ]
+            ]
+        , pre
+            [ style
+                [ ( "white-space", "pre-wrap" )
+                ]
+            ]
+            [ text (toString model) ]
         ]
-      ]
-      [ text (toString model) ]
-    ]
+
 
 
 -- SUBSCRIPTIONS
@@ -144,108 +147,136 @@ view model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+    Sub.none
+
 
 
 -- HTTP
 
-apiKey = "AIzaSyDZVUn4Hf7xI0arEW8_pmrVBO7hfjjP6sM"
+
+apiKey =
+    "AIzaSyDZVUn4Hf7xI0arEW8_pmrVBO7hfjjP6sM"
 
 
-api = "urlshortener"
-version = "v1"
-collection = "url"
-method = "get"
+api =
+    "urlshortener"
+
+
+version =
+    "v1"
+
+
+collection =
+    "url"
+
+
+method =
+    "get"
 
 
 driveCmd : Model -> Cmd Msg
 driveCmd model =
-  case model.token of
-    Just token ->
-      Task.perform
-        (always DriveResp)
-        (always DriveResp)
-        (send token drive [("q", "name contains 'test'")])
+    case model.token of
+        Just token ->
+            send token drive [ ( "q", "name contains 'test'" ) ]
 
-    Nothing -> Cmd.none
+        Nothing ->
+            Cmd.none
 
 
 newApiClient : ApiClient
 newApiClient =
-  { token = Nothing
-  }
+    { token = Nothing
+    }
 
 
 type alias ApiClient =
-  { token : Maybe String
-  }
+    { token : Maybe String
+    }
 
 
 type alias ApiDef =
-  { name : String
-  , version : String
-  , collection : String
-  , method : String
-  }
+    { name : String
+    , version : String
+    , collection : String
+    , method : String
+    }
 
 
 type Method
-  = Get (List (String, String))
-  | Post String
+    = Get (List ( String, String ))
+    | Post String
 
 
 urlShortener : ApiDef
 urlShortener =
-  { name = "urlshortener"
-  , version = "v1"
-  , collection = "url"
-  , method = "get"
-  }
+    { name = "urlshortener"
+    , version = "v1"
+    , collection = "url"
+    , method = "get"
+    }
 
 
 drive : ApiDef
 drive =
-  { name = "drive"
-  , version = "v3"
-  , collection = "files"
-  , method = "get"
-  }
+    { name = "drive"
+    , version = "v3"
+    , collection = "files"
+    , method = "get"
+    }
 
 
-req : OAuth.Token -> ApiDef -> List (String, String) -> Http.Request
+req : OAuth.Token -> ApiDef -> List ( String, String ) -> Http.Request String
 req token def fields =
-  { verb = def.method
-  , headers = getHeaders token
-  , url =
-    Http.url
-      ("https://content.googleapis.com/" ++ def.name ++ "/" ++ def.version ++ "/" ++ def.collection)
-      (fields ++ [("key", apiKey)])
-  , body = Http.empty
-  }
+    Http.request
+        { method = def.method
+        , headers = getHeaders token
+        , url =
+            url
+                ("https://content.googleapis.com/" ++ def.name ++ "/" ++ def.version ++ "/" ++ def.collection)
+                (fields ++ [ ( "key", apiKey ) ])
+        , body = Http.emptyBody
+        , expect = Http.expectString
+        , timeout = Nothing
+        , withCredentials = False
+        }
 
 
-getHeaders : OAuth.Token -> List (String, String)
+getHeaders : OAuth.Token -> List Http.Header
 getHeaders token =
-  case token of
-    OAuth.Validated t -> [("Authorization", "Bearer " ++ t)]
+    case token of
+        OAuth.Validated t ->
+            [ Http.header "Authorization" ("Bearer " ++ t) ]
 
 
-send : OAuth.Token -> ApiDef -> List (String, String) -> Task.Task Http.Error String
+send : OAuth.Token -> ApiDef -> List ( String, String ) -> Cmd Msg
 send token def fields =
-  Http.send Http.defaultSettings (req token def fields)
-    |> Task.mapError promoteError
-    |> Task.map (\resp -> stringValue resp.value)
+    Http.send handle (req token def fields)
 
 
-promoteError : Http.RawError -> Http.Error
-promoteError rawError =
-  case rawError of
-    Http.RawTimeout -> Http.Timeout
-    Http.RawNetworkError -> Http.NetworkError
+handle r =
+    case r of
+        Ok v ->
+            Nop
+
+        Err _ ->
+            Nop
 
 
-stringValue : Http.Value -> String
-stringValue v =
-  case v of
-    Http.Text str -> str
-    _ -> ""
+
+-- Copied from https://github.com/evancz/elm-http/blob/master/src/Http.elm#L56
+
+
+url : String -> List ( String, String ) -> String
+url baseUrl args =
+    case args of
+        [] ->
+            baseUrl
+
+        _ ->
+            baseUrl ++ "?" ++ String.join "&" (List.map queryPair args)
+
+
+queryPair : ( String, String ) -> String
+queryPair ( key, value ) =
+    Http.encodeUri key ++ "=" ++ Http.encodeUri value
--- src/OAuth.elm
+++ src/OAuth.elm
@@ -1,12 +1,13 @@
-module OAuth exposing
-  ( Client
-  , ServerConfig
-  , ClientConfig
-  , Token (..)
-  , buildAuthUrl
-  , newClient
-  , urlParser
-  )
+module OAuth
+    exposing
+        ( Client
+        , ServerConfig
+        , ClientConfig
+        , Token(..)
+        , init
+        , buildAuthUrl
+        , newClient
+        )
 
 {-| This library allows handling OAuth 2.0 authentication.
 
@@ -38,34 +39,34 @@ import Task
 {-| Represents a validated OAuth token.
 -}
 type Token
-  = Validated String
+    = Validated String
 
 
 {-| Server-side configuration for a single OAuth client.
 It includes the endpoints used to obtain and verify tokens, and also client-specific settings.
 -}
 type alias ServerConfig =
-  { endpointUrl : String
-  , validateUrl : String
-  }
+    { endpointUrl : String
+    , validateUrl : String
+    }
 
 
 {-| Client-side configuration for a single OAuth client.
 It includes the endpoints used to obtain and verify tokens, and also client-specific settings.
 -}
 type alias ClientConfig =
-  { clientId : String
-  , scopes : List String
-  , redirectUrl : String
-  }
+    { clientId : String
+    , scopes : List String
+    , redirectUrl : String
+    }
 
 
 {-| An OAuth client.
 -}
 type alias Client =
-  { serverConfig : ServerConfig
-  , clientConfig : ClientConfig
-  }
+    { serverConfig : ServerConfig
+    , clientConfig : ClientConfig
+    }
 
 
 {-| Creates a new OAuth client based on a server configuration and a client configuration.
@@ -77,83 +78,100 @@ Normally defined at top-level in the application.
 -}
 newClient : ServerConfig -> ClientConfig -> Client
 newClient serverConfig clientConfig =
-  { serverConfig = serverConfig
-  , clientConfig = clientConfig
-  }
+    { serverConfig = serverConfig
+    , clientConfig = clientConfig
+    }
 
 
 {-| Builds an URL that when followed allows the user to authenticate with the specified provider.
 -}
+
+
+
 -- TODO: Generate and verify nonce.
+
+
+init : Client -> Navigation.Location -> Cmd (Result Http.Error Token)
+init client =
+    .hash >> getTokenFromHash >> validateToken client
+
+
 buildAuthUrl : Client -> String
 buildAuthUrl client =
-  Http.url
-    client.serverConfig.endpointUrl
-    [ ("response_type", "token")
-    , ("immediate", "true")
-    , ("approval_prompt", "auto")
-    , ("client_id", client.clientConfig.clientId)
-    , ("redirect_uri", client.clientConfig.redirectUrl)
-    , ("scope", String.join " " client.clientConfig.scopes)
-    ]
+    url
+        client.serverConfig.endpointUrl
+        [ ( "response_type", "token" )
+        , ( "immediate", "true" )
+        , ( "approval_prompt", "auto" )
+        , ( "client_id", client.clientConfig.clientId )
+        , ( "redirect_uri", client.clientConfig.redirectUrl )
+        , ( "scope", String.join " " client.clientConfig.scopes )
+        ]
 
 
 buildValidateUrl : Client -> String -> String
 buildValidateUrl client token =
-  Http.url
-    client.serverConfig.validateUrl
-    [ ("input_token", token)
-    , ("access_token", token)
-    ]
-
-
-{-| A function to create a URL parser to be used with a `Navigation.program`.
-
-    main =
-      Navigation.program (OAuth.urlParser authClient)
-        { init = ...
-        , view = ...
-        , update = ...
-        , urlUpdate = ...
-        , subscriptions = ...
-        }
-
--}
-urlParser : Client -> Navigation.Parser (Task.Task String Token)
-urlParser client =
-  Navigation.makeParser (.hash >> getTokenFromHash >> validateToken client)
+    url
+        client.serverConfig.validateUrl
+        [ ( "input_token", token )
+        , ( "access_token", token )
+        ]
 
 
 getTokenFromHash : String -> String
 getTokenFromHash s =
-  let
-    params = parseUrlParams s
-  in
-    Dict.get "access_token" params
-      |> Maybe.withDefault ""
+    let
+        params =
+            parseUrlParams s
+    in
+        Dict.get "access_token" params
+            |> Maybe.withDefault ""
 
 
 parseUrlParams : String -> Dict.Dict String String
 parseUrlParams s =
-  s
-    |> String.dropLeft 1
-    |> String.split "&"
-    |> List.map parseSingleParam
-    |> Dict.fromList
+    s
+        |> String.dropLeft 1
+        |> String.split "&"
+        |> List.map parseSingleParam
+        |> Dict.fromList
 
 
-parseSingleParam : String -> (String, String)
+parseSingleParam : String -> ( String, String )
 parseSingleParam p =
-  let
-    s = String.split "=" p
-  in
-    case s of
-      [s1,s2] -> (s1,s2)
-      _ -> ("", "")
+    let
+        s =
+            String.split "=" p
+    in
+        case s of
+            [ s1, s2 ] ->
+                ( s1, s2 )
+
+            _ ->
+                ( "", "" )
 
 
-validateToken : Client -> String -> Task.Task String Token
+validateToken : Client -> String -> Cmd (Result Http.Error Token)
 validateToken client token =
-  Http.getString (buildValidateUrl client token)
-    |> Task.mapError (always "error")
-    |> Task.map (always (Validated token))
+    Http.getString (buildValidateUrl client token)
+        |> Http.send identity
+        |> Cmd.map (Result.map Validated)
+
+
+
+-- Copied from https://github.com/evancz/elm-http/blob/master/src/Http.elm#L56
+
+
+url : String -> List ( String, String ) -> String
+url baseUrl args =
+    case args of
+        [] ->
+            baseUrl
+
+        _ ->
+            baseUrl ++ "?" ++ String.join "&" (List.map queryPair args)
+
+
+queryPair : ( String, String ) -> String
+queryPair ( key, value ) =
+    Http.encodeUri key ++ "=" ++ Http.encodeUri value
